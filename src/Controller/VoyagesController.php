f54ac67c4491aa12bc67e0ff4c7da062ea635a7f
Tom <tomaloys.pro@gmail.com>
2025-02-06 18:51:42 +0100

    phase 9 : ORM (filtrer)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\VisiteRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class VoyagesController extends AbstractController {
     /**
@@ -23,11 +24,29 @@ class VoyagesController extends AbstractController {
     #[Route('/voyages', name: 'voyages')]
     public function index(): Response {
         // Récupérer les données depuis la base
-        $visites = $this->repository->findAll();
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
 
         // Envoyer les données à la vue
         return $this->render("pages/voyages.html.twig", [
             'visites' => $visites
         ]);
+    }
+    
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+                 
+    }
+    
+    #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal')]
+    public function findAllEqual($champ, Request $request): Response{
+        $valeur = $request->get("recherche");
+        $visites = $this->repository->findByEqualValue($champ, $valeur);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+                ]);
     }
 }
